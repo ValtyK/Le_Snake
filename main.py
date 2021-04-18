ede6eec09ccb9bc0a69e92b99aab21875e122bad
ValtyK <>
2021-04-18 17:26:09 +0000

    always work on "affichage_aire_de_jeu" function

--- main.py
+++ main.py
@@ -14,31 +14,47 @@ def affichage_titre(titre):
     print(i)
     time.sleep(0.5)
 
-affichage_titre(titre)
 
 # création de la fenêtre
 def affichage_aire_de_jeu(hauteur, largeur, titre):
     # Création d'une nouvelle fenètre en 0, 0
-    win = curses.____
+    win = curses.newwin(0, 0)
     # Les séquences d'échapement sont générés par certaines touches, les autres n'ont aucun effet
-    win.keypad(__)
+    win.keypad(True)
     # L'écho des caractères saisis est désactivé
     curses.noecho()
     # Pas de curseur visible
-    curses.curs_set(__)
+    curses.curs_set(False)
     # La saisie de caractère est non bloquante
     win.nodelay(1)
     # La fenètre a une bordure standard
-    win.____
+    win.border(1)
     # Définition d'une couleur pour le titre : texte en rouge sur fond blanc
     # Voir dans la documentation la table "lists the predefined colors"
-    curses.init_pair(1, curses.____, curses.____)
+    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_WHITE)
     # Affichage du titre
     win.addstr(0, 27, titre, curses.color_pair(1))
     # Raffraichissement de la fenêtre
-    win.____
+    win.refresh(1)
     # Emission d'un beep
-    curses.____
+    curses.beep()
     # retourner la fenêtre
-    return ____
+    return win
 
+#affichage du titre
+affichage_titre(titre)
+
+#initianilation du terminal
+curses.initscr()
+
+#démarrage du mode couleur
+curses.start_color()
+
+#affichage de l'air de jeu
+affichage_aire_de_jeu(20,60,"SNAKE")
+
+#Tempo 10 secondes
+curses.napms(10000)
+
+#fin de l'affichage
+curses.endwin()
